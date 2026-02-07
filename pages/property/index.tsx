da33e1073a253a4ac6fa363a4128bc718aa8e34f
yakuboff-m <m.yakuboff.15@gmail.com>
2026-02-08 00:54:56 +0900

    feat: develop query logic on property list component

--- pages/property/index.tsx
+++ pages/property/index.tsx
@@ -11,6 +11,9 @@ import { Property } from '../../libs/types/property/property';
 import { serverSideTranslations } from 'next-i18next/serverSideTranslations';
 import KeyboardArrowDownRoundedIcon from '@mui/icons-material/KeyboardArrowDownRounded';
 import { Direction } from '../../libs/enums/common.enum';
+import { T } from '../../libs/types/common';
+import { GET_PROPERTIES } from '../../apollo/user/query';
+import { useQuery } from '@apollo/client';
 
 export const getStaticProps = async ({ locale }: any) => ({
 	props: {
@@ -32,6 +35,15 @@ const PropertyList: NextPage = ({ initialInput, ...props }: any) => {
 	const [filterSortName, setFilterSortName] = useState('New');
 
 	/** APOLLO REQUESTS **/
+	const { loading: getPropertiesLoading, data: getPropertiesData, error: getPropertiesError, refetch: getPropertiesRefetch } = useQuery(GET_PROPERTIES, {
+			fetchPolicy: "network-only",
+			variables: {input: searchFilter},
+			notifyOnNetworkStatusChange: true,
+			onCompleted: (data: T) => {
+				setProperties(data?.getProperties?.list);
+				setTotal(data?.getProperties?.metaCounter[0]?.total);
+			},
+		}) ;
 
 	/** LIFECYCLES **/
 	useEffect(() => {
@@ -43,7 +55,9 @@ const PropertyList: NextPage = ({ initialInput, ...props }: any) => {
 		setCurrentPage(searchFilter.page === undefined ? 1 : searchFilter.page);
 	}, [router]);
 
-	useEffect(() => {}, [searchFilter]);
+	useEffect(() => {
+		console.log("searchFilter:", searchFilter);
+	}, [searchFilter]);
 
 	/** HANDLERS **/
 	const handlePaginationChange = async (event: ChangeEvent<unknown>, value: number) => {

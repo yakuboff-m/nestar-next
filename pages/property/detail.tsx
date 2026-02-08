15438c6f42566fa023f2a812ef26728fa163b535
yakuboff-m <m.yakuboff.15@gmail.com>
2026-02-09 01:11:36 +0900

    feat: develop property related comments data

--- pages/property/detail.tsx
+++ pages/property/detail.tsx
@@ -32,6 +32,7 @@ import { T } from '../../libs/types/common';
 import { Direction, Message } from '../../libs/enums/common.enum';
 import { LIKE_TARGET_PROPERTY } from '../../apollo/user/mutation';
 import { sweetMixinErrorAlert, sweetTopSmallSuccessAlert } from '../../libs/sweetAlert';
+import { GET_COMMENTS } from '../../apollo/admin/query';
 
 SwiperCore.use([Autoplay, Navigation, Pagination]);
 
@@ -81,7 +82,7 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 				sort: 'createdAt',
 				direction: Direction.DESC,
 				search: {
-					locationList: [property?.propertyLocation]
+					locationList: property?.propertyLocation ? [property?.propertyLocation] : [],
 				},
 			}
 		},
@@ -91,6 +92,19 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 			if (data?.getProperties?.list) {
 				setDestinationProperties(data?.getProperties?.list);
 			}
+		},
+	});
+
+		const { loading: getCommentsLoading, data: getCommentsData, error: getCommentsError, refetch: getCommentsRefetch } = useQuery(GET_COMMENTS, {
+		fetchPolicy: "cache-and-network",
+		variables: {input: initialComment},
+		skip: !commentInquiry?.search?.commentRefId,
+		notifyOnNetworkStatusChange: true,
+		onCompleted: (data: T) => {
+			if (data?.getComments?.list) {
+				setPropertyComments(data?.getComments?.list);
+			}
+			setCommentTotal(data?.getComments?.metaCounter[0]?.total ?? 0);
 		},
 	});
 
@@ -111,7 +125,9 @@ const PropertyDetail: NextPage = ({ initialComment, ...props }: any) => {
 		}
 	}, [router]);
 
-	useEffect(() => { }, [commentInquiry]);
+	useEffect(() => { 
+		if(commentInquiry?.search?.commentRefId) getCommentsRefetch({input: commentInquiry});
+	}, [commentInquiry]);
 
 	/** HANDLERS **/
 	const changeImageHandler = (image: string) => {

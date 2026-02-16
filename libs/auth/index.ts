6e2fe6d8b066235849294a323ca4cd98c4eb6790
yakuboff-m <m.yakuboff.15@gmail.com>
2026-02-17 02:14:48 +0900

    feat: Integration of GraphQL Error handling for client application

--- libs/auth/index.ts
+++ libs/auth/index.ts
@@ -26,7 +26,7 @@ export const logIn = async (nick: string, password: string): Promise<void> => {
 	} catch (err) {
 		console.warn('login err', err);
 		logOut();
-		throw new Error('Login Err');
+		// throw new Error('Login Err');
 	}
 };
 
